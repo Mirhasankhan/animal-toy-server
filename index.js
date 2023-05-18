83b97ef0ed8c741b857eca525c2c654d3a287afb
Mirhasankhan <mirhasan000034@gmail.com>
2023-05-18 23:37:04 +0600

    single data get

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('dotenv').config()
 const app = express();
@@ -30,6 +30,13 @@ async function run() {
 
         app.get('/addToy', async(req, res)=>{
             const result = await animalCollection.find().toArray();
+            res.send(result)
+        })
+
+        app.get('/addToy/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id : new ObjectId(id)}
+            const result = await animalCollection.findOne(query)
             res.send(result)
         })
 
